1fade041b93f3a9511dc5881702ea34b6f3945b0
xowoony <trumanblack@hanmail.net>
2023-04-24 18:14:43 +0900

    selector 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useRecoilState } from "recoil";
-import { minuteState } from "./atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
 import { useState } from "react";
 
 function App() {
   // useRecoilState는 atom의 값과 그 atom을 수정할 수 있는 함수를 줌
   const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);//+를 적어주면 string=>number가 됨
+    setMinutes(+event.currentTarget.value); //+를 적어주면 string=>number가 됨
   };
-  
 
   return (
     <div>
@@ -18,12 +18,7 @@ function App() {
         type="number"
         placeholder="Minutes"
       />
-      <input type="number" placeholder="Hours" />
-      <div>
-      <span>Minutes : {minutes} </span>
-      <span>Hours : {minutes/60}</span>
-      </div>
-
+      <input value={hours} type="number" placeholder="Hours" />
     </div>
   );
 }
