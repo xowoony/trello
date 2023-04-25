91404a3ae3ae8655e055d0360854f693d167b3f2
xowoony <trumanblack@hanmail.net>
2023-04-25 17:51:35 +0900

    DragDropContext, Droppable, Draggable 작성하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 function App() {
   const onDragEnd = () => {};
-  return <DragDropContext onDragEnd={onDragEnd}>
-    <span>하하</span>
-  </DragDropContext>;
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {() => (
+            <ul>
+              <Draggable draggableId="first" index={0}>
+                {() => <li>One</li>}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {() => <li>Two</li>}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 }
 
 export default App;
