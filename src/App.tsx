68b3c48d496485c9daa99a7cbed5e792ec1fd8c2
xowoony <trumanblack@hanmail.net>
2023-04-26 17:14:46 +0900

    css 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  margin: 0 auto;
+  max-width: 470px;
+  width: 100%;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  width: 100%;
+`;
+
 const Board = styled.div`
+  border-radius: 5px;
   background-color: ${(props) => props.theme.boardColor};
+  padding: 20px 10px;
+  padding-top: 30px;
+  min-height: 200px;
 `;
 
 const Card = styled.div`
+  border-radius: 5px;
   background-color: ${(props) => props.theme.cardColor};
+  padding: 10px 10px;
 `;
 
 function App() {
   const onDragEnd = () => {};
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div>
-        <Droppable droppableId="one">
-          {(magic) => (
-            <Board ref={magic.innerRef} {...magic.droppableProps}>
-              <Draggable draggableId="first" index={0}>
-                {(magic) => (
-                  <Card ref={magic.innerRef} {...magic.draggableProps}>
-                    <span {...magic.dragHandleProps}>★</span>
-                    one
-                  </Card>
-                )}
-              </Draggable>
-            </Board>
-          )}
-        </Droppable>
-      </div>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {(magic) => (
+              <Board ref={magic.innerRef} {...magic.droppableProps}>
+                <Draggable draggableId="first" index={0}>
+                  {(magic) => (
+                    <Card
+                      ref={magic.innerRef}
+                      {...magic.draggableProps}
+                      {...magic.dragHandleProps}
+                    >
+                      one
+                    </Card>
+                  )}
+                </Draggable>
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
     </DragDropContext>
   );
 }
