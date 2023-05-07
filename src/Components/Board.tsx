aaffec78c87e0eb09cc4912699690e542e967b68
xowoony <trumanblack@hanmail.net>
2023-05-07 13:36:39 +0900

    각각의 보드에서 card 메모 추가하기 완료

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -2,7 +2,8 @@ import { Droppable } from "react-beautiful-dnd";
 import DragabbleCard from "./DragabbleCard";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
-import { ITodo } from "../atoms";
+import { ITodo, toDoState } from "../atoms";
+import { useSetRecoilState } from "recoil";
 
 // 보드
 const Wrapper = styled.div`
@@ -66,11 +67,29 @@ interface IForm {
 // boardId를 넘겨주고 밑에서 DroppableId로 boardId를 주도록 한다.
 // 이렇게 하면 재사용할 수 있는 board 컴포넌트가 생겼다.
 function Board({ toDos, boardId }: IBoardProps) {
+  // state를 조작할 수 있는 함수
+  const setToDos = useSetRecoilState(toDoState);
   const { register, setValue, handleSubmit } = useForm<IForm>();
+
   // form이 valid할 때 호출되는 함수
   const onValid = ({ toDo }: IForm) => {
+    const newToDo = {
+      id: Date.now(),
+      text: toDo,
+    };
+    setToDos((allBoards) => {
+      return {
+        ...allBoards,
+        [boardId]: [newToDo, ...allBoards[boardId] ],
+      };
+    }); // 현재 내가 있는 보드에만 올려주어야 함.
+    // toDo를 Done 보드에 넣고 싶다고 했을 때
+    // Done을 제외한 나머지는 제자리에 그대로 두고
+    // Done안에도 그대로 두고 내 값만 마지막에 붙여주어야 함.
+    // 그럴려면 현재 보드를 사용해서 카피하고 그걸 다시 state에 넣어야 함.
     setValue("toDo", "");
   };
+
   return (
     <Wrapper>
       <Title>{boardId}</Title>
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -11,17 +11,15 @@ interface IToDoState {
   [key: string]: ITodo[];
 }
 
+// setter함수
 export const toDoState = atom<IToDoState>({
   key: "toDo",
   // default를 기존의 배열에서 -> object로 변경해준다.
   default: {
     // 이 to_do는 배열을 가진다.
-    "진행 예정": [
-      { id: 1, text: "hello" },
-      { id: 2, text: "hello" },
-    ],
+    "진행 예정": [],
     "진행 중": [],
-    완료: [],
+    "완료": [],
   },
 });
 
